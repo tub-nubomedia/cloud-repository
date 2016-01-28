0e15ba7d79c25a37fc93dd9235a8936be5daf489
lorenzo.tomasini <lorenzo.tomasini@fokus.fraunhofer.de>
2016-01-28 16:08:49 +0100

    added optional parameters and fixed tutorial

--- tutorial/connect_from_remote.py
+++ tutorial/connect_from_remote.py
@@ -8,29 +8,42 @@ __author__ = 'lto'
 
 
 def usage():
-    return "python connect_from_remote.py [-u <username>] [-d] [--password] --ip <CLOUDREPO_IP>"
+    return "python connect_from_remote.py [-u <username>] [-d] [--password] --ip <CLOUDREPO_IP>\n\nBasically performing \'mongo --host $IP --port $PORT -u $USERNAME -p $PASSWORD --authenticationDatabase \"admin\""
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Cloud Repository Conncetor', usage=usage)
-    parser.add_argument('-u', '--username', help='username for the cloud repository, default is \'nubomedia\'')
+    parser = argparse.ArgumentParser(description='Cloud Repository Conncetor', usage=usage())
+    parser.add_argument('-u', '--username', help='username for the cloud repository')
     parser.add_argument('-pwd', '--password', help='password for the given username')
-    parser.add_argument('-d', '--debug', help='activate debug', action='store_true')
-    parser.add_argument('--ip', help='ip of the cloud repository', required=True)
+    parser.add_argument('-i', '--ip', help='ip of the cloud repository')
+    parser.add_argument('-p', '--port', help='port of the cloud repository')
 
     args = vars(parser.parse_args(sys.argv[1:]))
 
-    password=''
-
-    if args.get("password") is None:
-        print "Insert the password for the cloud repository:\t"
-        password = urllib.quote_plus(getpass.getpass())
+    ip=''
+    if args.get("ip") is None:
+        ip='localhost'
     else:
-        password = urllib.quote_plus(args.get("password"))
+        ip=args.get("ip")
+    port=''
+    if args.get("port") is None:
+        port='27018'
+    else:
+        port=args.get("port")
 
-    # mongo --port 27018 -u "myUserAdmin" -p "abc123" --authenticationDatabase "admin"
+    password=''
+    if args.get("username") is not None:
+        if args.get("password") is None:
+            print "Insert the password for the cloud repository:\t"
+            password = urllib.quote_plus(getpass.getpass())
+        else:
+            password = urllib.quote_plus(args.get("password"))
 
-    client = MongoClient('mongodb://%s:%s@%s:27018' % (args.get("username"), password, args.get("ip")))
+	    client = MongoClient('mongodb://%s:%s@%s:%s' % (args.get("username"), password, ip, port))
+    else:
+	    client = MongoClient(ip, int(port))
+
+    print 'Connected to %s' % client 
 
     print "Database are: "
     print client.database_names()
